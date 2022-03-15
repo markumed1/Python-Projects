9654bbe514be01e8f9ee660075941f0ac92349e6
Mark Medrano <markumed1@hotmail.com>
2022-03-15 13:30:03 -1000

    assignment

---
+++ Course Assignments/WebPageGenerator.py
@@ -0,0 +1,5 @@
+f = open("myfile.txt", "a")
+f.write("Stay tuned for our amazing summer sale!")
+f.close()
+f = open("myfile.txt", "r")
+print(f.read())
--- Course Assignments/widget_challenge.py
+++ Course Assignments/widget_challenge.py
@@ -9,6 +9,7 @@ errmsg = 'Error!'
 tk.Button(text='Click to Open File', 
        command=callback).pack(fill=tk.X)
 tk.mainloop()
+askdirectory()
 
 
 
