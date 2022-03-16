ec1386f33e1ed881f6a13d17065915d51bf2da25
Mark Medrano <markumed1@hotmail.com>
2022-03-16 09:33:03 -1000

    assignment update

--- Course Assignments/WebPageGenerator2.py
+++ Course Assignments/WebPageGenerator2.py
@@ -1,6 +1,35 @@
-# Import tkinter module
+import webbrowser
 import tkinter
 
+
+# enter message with """ """
+message = """<html>
+  <body>
+   <h1>
+      Stay tuned for our amazing summer sale!
+    </h1>
+  </body>
+</html>"""
+
+
+# create function to set the text with the help of button.
+def set_text_by_button():
+
+    # "w" Write - will overwrite any existing content
+    f = open('staytuned.html', 'w')
+    message = sample_text.get()
+    print (message)
+   
+    f.write(message)
+    f.close()
+    webbrowser.open_new_tab('staytuned.html')
+    
+    # Delete is going to erase anything
+    # The respective range given here 
+    # in the range of 0 and end of file,
+    sample_text.delete(0,"end")
+    
+# Set up the GUI
 # Create GUI window
 window = tkinter.Tk()
 window.title("Welcome fellow guests")
@@ -9,21 +38,14 @@ window.geometry("400x200")
 # creating text widget.
 sample_text = tkinter.Entry(window)
 sample_text.pack()
-
-# create function to set the text with the help of button.
-def set_text_by_button():
-
-   # Delete is going to erase anything
-    # in the range of 0 and end of file,
-    # The respective range given here
-    sample_text.delete(0,"end")
-    
-  
 # Setting up the button, set_text_by_button() 
 # is passed as a command
 set_up_button = tkinter.Button(window, height=1, width=10, text="Enter", 
                     command=set_text_by_button)
-  
 set_up_button.pack()
-  
-window.mainloop()
+
+if __name__ == "__main__":
+    window.mainloop()
+
+
+
