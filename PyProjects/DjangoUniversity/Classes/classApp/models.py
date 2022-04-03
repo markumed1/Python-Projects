a92130c77be1aca9c771f5607b232d0c78a45270
Mark Medrano <markumed1@hotmail.com>
2022-04-02 14:43:46 -1000

    Assignment update

--- PyProjects/DjangoUniversity/Classes/classApp/models.py
+++ PyProjects/DjangoUniversity/Classes/classApp/models.py
@@ -1,18 +1,15 @@
 # import models top of page before creating class
+from datetime import timedelta
+
 from django.db import models
 
-TITLE_CHOICES = {
-    ('MR', 'Mr.'),
-    ('MRS', 'Mrs.'),
-    ('MS', 'Ms.'),
-}
 
-
+# write your models
 class DjangoClasses(models.Model):
-    title = models.CharField(max_length=5)
-    course_number = models.CharField(max_length=10)
-    instructor_name = models.CharField(max_length=30)
-    duration = models.CharField(max_length=10)
+    title = models.CharField(max_length=60)
+    course_number = models.IntegerField(blank=True, null=True)
+    instructor_name = models.CharField(max_length=60)
+    duration = models.DurationField(default=timedelta())
 
     #   is the default model manager and each model needs to have at least ONE model manager
     objects = models.Manager()
