214535fe16d3ee38a2207f8b24638b9f543c9cb4
Mark Medrano <markumed1@hotmail.com>
2022-04-02 15:39:17 -1000

    update

--- PyProjects/DjangoProject/Django_Checkbook/Checkbook/views.py
+++ PyProjects/DjangoProject/Django_Checkbook/Checkbook/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+def create_account(request):
+    return render(request, 'checkbook/CreateNewAccount.html')
+
+def balance(request):
+    return render(request, 'checkbook/BalanceSheet.html')
+
+def transaction(request):
+    return render(request, 'checkbook/AddTransaction.html')
